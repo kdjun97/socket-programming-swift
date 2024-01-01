0bea3cd81a61e52771deb6e357f852e19d4d8629
kdjun97 <kdjun97@gmail.com>
2024-01-01 20:52:04 +0900

    init project

---
+++ SocketProgrammingSwift/SocketProgrammingSwift/MyApp.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SocketProgrammingSwift
+//
+//  Created by 김동준 on 1/1/24
+//
+
+import SwiftUI
+
+struct MyApp: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Socket Programming Test!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MyApp()
+}
---
+++ SocketProgrammingSwift/SocketProgrammingSwift/SocketProgrammingSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocketProgrammingSwiftApp.swift
+//  SocketProgrammingSwift
+//
+//  Created by 김동준 on 1/1/24
+//
+
+import SwiftUI
+
+@main
+struct SocketProgrammingSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MyApp()
+        }
+    }
+}
